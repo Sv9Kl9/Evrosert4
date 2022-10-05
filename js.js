fc4283ffc8b057f17b8953269c448e5330d119c7
SvitlanaH <91554698+Sv9Kl9@users.noreply.github.com>
2022-10-05 22:32:37 +0300

    first

---
+++ js.js
@@ -0,0 +1,20 @@
+// let formButton = document.getElementById('form-button');
+// let formName = document.getElementById('cost-form-name');
+// let formPhone = document.getElementById('cost-form-phone');
+// let formEmail = document.getElementById('cost-form-email');
+// let formProducts = document.getElementById('cost-form-products');
+// let formCode = document.getElementById('cost-form-code');
+// let formDocument = document.getElementById('cost-form-documents');
+// let formInfo = document.getElementById('cost-form-textarea');
+
+// formButton.addEventListener('click', () => {
+//   formName.value='';
+//   formPhone.value='';
+//   formEmail.value='';
+//   formProducts.value='';
+//   formCode.value='';
+//   formDocument.value='';
+//   formInfo.value='';
+  
+// })
+
