ea5668e121c7cc1d89e9738d1f5a9591c44cc75c
SvitlanaH <91554698+Sv9Kl9@users.noreply.github.com>
2022-10-06 15:26:55 +0300

    fixSite

--- js.js
+++ js.js
@@ -1,25 +1,6 @@
-// let formButton = document.getElementById('form-button');
-// let formName = document.getElementById('cost-form-name');
-// let formPhone = document.getElementById('cost-form-phone');
-// let formEmail = document.getElementById('cost-form-email');
-// let formProducts = document.getElementById('cost-form-products');
-// let formCode = document.getElementById('cost-form-code');
-// let formDocument = document.getElementById('cost-form-documents');
-// let formInfo = document.getElementById('cost-form-textarea');
-
-// formButton.addEventListener('click', () => {
-//   formName.value='';
-//   formPhone.value='';
-//   formEmail.value='';
-//   formProducts.value='';
-//   formCode.value='';
-//   formDocument.value='';
-//   formInfo.value='';
-  
-// })
-
 let header = document.getElementById('header')
 let screenWidth = window.screen.width
+let screenHeight = window.screen.height
 let logo = document.getElementById('logo-wrap')
 let navBar = document.getElementById('navbarNavAltMarkup')
 let imgOver = document.getElementById('about-img-over')
