0ffed91ca432cad89d293e41d3fb57dfb5d3ce92
SvitlanaH <91554698+Sv9Kl9@users.noreply.github.com>
2022-10-06 13:35:53 +0300

    withoutImg

--- js.js
+++ js.js
@@ -24,7 +24,7 @@ let logo = document.getElementById('logo-wrap')
 let navBar = document.getElementById('navbarNavAltMarkup')
 let imgOver = document.getElementById('about-img-over')
 
-if(screenWidth>2200){
+if(screenWidth>2300){
   header.style.backgroundImage = "url('img/cover2000.png')";
   logo.style.marginLeft = '230px'
   navBar.style.marginRight = '200px'
